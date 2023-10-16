f2e803ead0816caf5942615ce2f266b26f808ae1
BillStephens2022 <two4onebill@yahoo.com>
2023-10-16 13:48:25 -0400

    check for existing user on sign up

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -22,12 +22,19 @@ async function handler(req, res) {
 
     const hashedPassword = await hashPassword(password);
 
-    const result = db.collection("users").insertOne({
+    const existingUser = await db.collection("users").findOne({email: email});
+    if (existingUser) {
+        res.status(422).json({ message: "User exists already!"});
+        client.close();
+        return;
+    }
+    const result = await db.collection("users").insertOne({
       email: email,
       password: hashedPassword,
     });
 
     res.status(201).json({ message: "Created User!" });
+    client.close();
   }
 }
 

3d9caee855344060183bebe809ce7d2bc6e7d8da
BillStephens2022 <two4onebill@yahoo.com>
2023-10-16 15:10:37 -0400

    added route protection to the auth route

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -22,6 +23,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -40,7 +42,7 @@ function AuthForm() {
         password: enteredPassword,
       });
       if (!result.error) {
-        // set some auth state
+        router.replace("/profile");
       }
     } else {
       try {

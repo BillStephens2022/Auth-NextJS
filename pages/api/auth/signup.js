e4380e9422de95e4bfa589f4b707eaa26bef181b
BillStephens2022 <two4onebill@yahoo.com>
2023-10-16 13:13:43 -0400

    set up connection to mongodb

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,25 @@
-function handler() {
+import { connectToDatabase } from "../../../lib/db";
 
+async function handler(req, res) {
+  const data = req.body;
+  const { email, password } = data;
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message: "Invalid input - password should be at least 7 characters long.",
+    });
+    return;
+  }
+  const client = await connectToDatabase();
+  const db = client.db();
+  db.collection("users").insertone({
+    email: email,
+    password: password,
+  });
 }
 
 export default handler;
